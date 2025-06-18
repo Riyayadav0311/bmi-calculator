8fc8d63ab59e9090e8f63b61598f32821d346794
Riya Yadav <riyaryadav87@gmail.com>
2025-06-18 13:26:26 +0530

    Initial commit: BMI Calculator in HTML/CSS/JS

---
+++ script.js
@@ -0,0 +1,39 @@
+const msg=document.querySelector("#msg");
+const resultBtn=document.querySelector("#result");
+
+
+const calBmi=()=>{
+    const height=parseFloat(document.getElementById("input1").value);
+    const weight=parseFloat(document.getElementById("input2").value);
+   
+   if(height== null || height<=0 || weight == null || weight<=0){
+     msg.innerText="Your height and weight must be positive integer.";
+
+   }
+   else{
+    heightM = height/100;
+    let bmi=weight/(heightM*heightM);
+    bmi = Number(bmi.toFixed(2));
+    result(bmi);
+   }
+ 
+};
+const result=(bmi)=>{
+
+    if(bmi <18.5){
+        msg.innerText=`BMI is ${bmi} You are Underweight `;
+      
+    }else if(bmi <25){
+        msg.innerText=`BMI is ${bmi} Your body is fine!`;
+    }else if(bmi <30){
+        msg.innerText=`BMI is ${bmi} You are Overweight`;
+    }else{
+        msg.innerText=`BMI is ${bmi}You are Obese `;
+    }
+};
+
+resultBtn.addEventListener("click",() =>{
+  calBmi();
+
+});
+     
